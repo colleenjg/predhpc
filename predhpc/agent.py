Changed "moved" to "shifted". Fixed edge bug in PF width calculation. Reran experiments. Reran hyperparameter search. Clarified definition of width for Gaussian fields and relationship to width metrics. Added comments on global variables in params_util. Changed the wait parameter to 20 sec to match BTSP application time. Added random walk after targets are encountered for agents in openfield.

predhpc/agent.py
@@ -43,11 +43,11 @@ class ResetableAgent(riabAgent, ext_util.ParamsManagerMixin):
         "start_position": None,  # position to start trajectories from
         "reset_position": None,  # position to reset trajectories from
         "target_position": None,  # position to use as target
-        "wait_between_targets": 10,  # number of steps to wait between target reaching
+        "wait_between_same_target": 10,  # number of steps to wait before a same target can be reached a second time
         "reset_reached_within_tol_prop_to_speed_dt": None,  # proportion of current expected step size to use as reset tolerance
         "target_reached_within_tol_prop_to_speed_dt": None,  # proportion of current expected step size to use as target tolerance
         "fixed_direction": False,  # keep same direction (1D environment only)
-        "wait_at_end": 0,  # number of steps to wait at the end of a trajectory (1D environment only)
+        "wait_after_trajectory": 0,  # number of steps to wait after completing a trajectory (1D environment only)
     }
 
     List of properties (in addition to ratinabox.Agent properties):
@@ -95,11 +95,11 @@ class ResetableAgent(riabAgent, ext_util.ParamsManagerMixin):
         "start_position": None,  # position to start trajectories from
         "reset_position": None,  # position to reset trajectories from
         "target_position": None,  # position to use as target
-        "wait_between_targets": 10,  # number of steps to wait between target reaching
+        "wait_between_same_target": 10,  # number of steps to wait before a same target can be reached a second time
         "reset_reached_within_tol_prop_to_speed_dt": None,  # proportion of current expected step size to use as reset tolerance
         "target_reached_within_tol_prop_to_speed_dt": None,  # proportion of current expected step size to use as target tolerance
         "fixed_direction": False,  # keep same direction (1D environment only)
-        "wait_at_end": 0,  # number of steps to wait at the end of a trajectory (1D environment only)
+        "wait_after_trajectory": 0,  # number of steps to wait after completing a trajectory (1D environment only)
     }
 
     ignored_param_keys = list()  # type: list[str]
@@ -146,8 +146,8 @@ class ResetableAgent(riabAgent, ext_util.ParamsManagerMixin):
 
         if self.Environment.dimensionality == "2D":
             self.fixed_direction = False
-            self.wait_at_end = 0
-        self._waiting_at_end = 0
+            self.wait_after_trajectory = 0
+        self._waiting_after_trajectory = 0
 
         self._init_tolerances()
 
@@ -354,7 +354,7 @@ class ResetableAgent(riabAgent, ext_util.ParamsManagerMixin):
         self.trajectory_df.loc[idx, "time_total"] = self.t - start_time
 
         if self.Environment.D == 2:
-            self.trajectory_df["stop_position_y"] = self.pos[1]
+            self.trajectory_df.loc[idx, "stop_position_y"] = self.pos[1]
 
     def _init_target_df(self):
         """
@@ -795,7 +795,7 @@ class ResetableAgent(riabAgent, ext_util.ParamsManagerMixin):
         Attributes:
         - _target_object_idx (int): Index of target object in environment objects.
         - steps_before_checking_for_target (int): Number of steps to wait before
-            checking for target.
+            checking for the same target again.
         - target_position (1D np.ndarray or None): Target position.
 
         Args:
@@ -837,20 +837,20 @@ class ResetableAgent(riabAgent, ext_util.ParamsManagerMixin):
 
         self.steps_before_checking_for_target = 0
 
-    def move_target_position(self, move=None, move_x=None, move_y=None, prop=False):
+    def shift_target_position(self, shift=None, shift_x=None, shift_y=None, prop=False):
         """
-        self.move_target_position()
+        self.shift_target_position()
 
-        Move the target position by a specified amount.
+        Shift the target position by a specified amount.
 
         Args:
-        - move (float, optional): Amount to move the target position by. Move is
+        - shift (float, optional): Amount to shift the target position by. Shift is
             applied to all dimensions. Default is None.
-        - move_x (float, optional): Amount to move the target position in the x
+        - shift_x (float, optional): Amount to shift the target position in the x
             direction only, if environment is 2D. Default is None.
-        - move_y (float, optional): Amount to move the target position in the y
+        - shift_y (float, optional): Amount to shift the target position in the y
             direction only, if environment is 2D. Default is None.
-        - prop (bool, optional): Whether to move the target position by a proportion
+        - prop (bool, optional): Whether to shift the target position by a proportion
             of the environment extent. Default is False.
 
         Raises:
@@ -862,40 +862,40 @@ class ResetableAgent(riabAgent, ext_util.ParamsManagerMixin):
 
         if self.target_position is None:
             raise RuntimeError(
-                "Cannot move target position, as it is not set. "
+                "Cannot shift target position, as it is not set. "
                 "Use set_target_position() first."
             )
 
         new_target_position = self.target_position.copy()
 
         if self.Environment.D == 1:
-            if move_x is not None or move_y is not None:
+            if shift_x is not None or shift_y is not None:
                 raise ValueError(
-                    "In 1D environment, only use 'move', not move_x or move_y."
+                    "In 1D environment, only use 'shift', not shift_x or shift_y."
                 )
-            move_x = move
-        elif move is not None:
-            if move_x is not None or move_y is not None:
+            shift_x = shift
+        elif shift is not None:
+            if shift_x is not None or shift_y is not None:
                 raise ValueError(
-                    "In 2D environment, if 'move' is provided, it should be alone "
-                    "and not with 'move_x' or 'move_y'."
+                    "In 2D environment, if 'shift' is provided, it should be alone "
+                    "and not with 'shift_x' or 'shift_y'."
                 )
-            move_x = move
-            move_y = move
+            shift_x = shift
+            shift_y = shift
 
-        for i, move in enumerate([move_x, move_y]):
-            if move is None:
+        for i, shift in enumerate([shift_x, shift_y]):
+            if shift is None:
                 continue
             new = new_target_position[i]
-            if move is not None:
+            if shift is not None:
                 min_val, max_val = [
                     fct(self.Environment.extent[int(i * 2) : int(i * 2 + 2)])
                     for fct in (min, max)
                 ]
                 width = max_val - min_val
                 if prop:
-                    move = move * width
-                new_target_position[i] = (new - min_val + move_x) % width + min_val
+                    shift = shift * width
+                new_target_position[i] = (new - min_val + shift_x) % width + min_val
 
         self.set_target_position(new_target_position)
 
@@ -1227,7 +1227,7 @@ class ResetableAgent(riabAgent, ext_util.ParamsManagerMixin):
 
         Attributes:
         - steps_before_checking_for_target (int): Number of steps to wait before
-            checking for target.
+            checking for the target again.
 
         Returns:
         - target_reached (bool): Whether the agent has reached the target position,
@@ -1246,7 +1246,7 @@ class ResetableAgent(riabAgent, ext_util.ParamsManagerMixin):
                 self.target_position, self.target_reached_within_tol_prop_to_speed_dt  # type: ignore[attr-defined]
             )
             if target_reached:
-                self.steps_before_checking_for_target = self.wait_between_targets + 1  # type: ignore[attr-defined]
+                self.steps_before_checking_for_target = self.wait_between_same_target + 1  # type: ignore[attr-defined]
 
         return target_reached
 
@@ -1390,17 +1390,6 @@ class ResetableAgent(riabAgent, ext_util.ParamsManagerMixin):
 
         return reached_position_steps
 
-        # last_t_sec = -np.inf
-        # for step in steps:
-        #     if step == len(Pyrs.Agent.history["t"]):
-        #         if step - 1 in steps:
-        #             continue
-        #         else:
-        #             step = step - 1
-        #     t_sec = Pyrs.Agent.history["t"][step]
-        #     if t_sec < last_t_sec + min_t_sec:
-        #         continue
-
     def get_distances_from_target(self, norm=True):
         """
         self.get_distances_from_target()
@@ -1512,8 +1501,8 @@ class ResetableAgent(riabAgent, ext_util.ParamsManagerMixin):
 
         self.current_trajectory_length = 0
 
-        if self.wait_at_end > 0 and self._waiting_at_end == 0:
-            self._waiting_at_end = self.wait_at_end + 1
+        if self.wait_after_trajectory > 0 and self._waiting_after_trajectory == 0:
+            self._waiting_after_trajectory = self.wait_after_trajectory + 1
 
         self._add_new_trajectory_to_df()
 
@@ -1554,8 +1543,8 @@ class ResetableAgent(riabAgent, ext_util.ParamsManagerMixin):
             if self.check_if_trajectory_end_reached():
                 self.reset()
 
-        if self._waiting_at_end > 0:
-            self._waiting_at_end -= 1
+        if self._waiting_after_trajectory > 0:
+            self._waiting_after_trajectory -= 1
             new_pos = self.pos
 
         # check for a forced next position
@@ -2877,7 +2866,7 @@ class TAgent(ResetableAgent):
         - right_reset_position (1D np.ndarray): Right arm reset position for the agent.
         - start_position (1D np.ndarray): Start position of the agent.
         - steps_before_checking_for_target (int): Number of steps to wait before
-            checking for target.
+            checking for the target again.
         - target_position (1D np.ndarray): Target position of the agent.
         """
 
@@ -2972,18 +2961,18 @@ class TAgent(ResetableAgent):
                 else:
                     self.target_arm = None
 
-    def move_target_position(self, move, backward=False, prop=False):
+    def shift_target_position(self, shift, backward=False, prop=False):
         """
-        self.move_target_position()
+        self.shift_target_position(shift)
 
-        Move the target position by a specified amount.
+        Shifts the target position by a specified amount.
 
         Args:
-        - move (float, optional): Amount to move the target position by. Move is
+        - shift (float, optional): Amount to shift the target position by. Shift is
             applied to all dimensions. Default is None.
-        - backward (bool, optional): Whether to move the target position backwards
+        - backward (bool, optional): Whether to shift the target position backwards
             along the T-maze instead of forward. Default is False.
-        - prop (bool, optional): Whether to move the target position by a proportion
+        - prop (bool, optional): Whether to shift the target position by a proportion
             of the environment extent. Default is False.
 
         Raises:
@@ -2998,12 +2987,12 @@ class TAgent(ResetableAgent):
 
         if self.target_position is None:
             raise RuntimeError(
-                "Cannot move target position, as it is not set. "
+                "Cannot shift target position, as it is not set. "
                 "Use set_target_position() first."
             )
 
         if prop:
-            move = move * self.Environment.scale
+            shift = shift * self.Environment.scale
 
         new_target_position = self.target_position.copy()
 
@@ -3011,8 +3000,8 @@ class TAgent(ResetableAgent):
 
         if backward:
             if new_target_position[0] == mid_pt_x:
-                if new_target_position[1] - move >= self.start_position[1]:
-                    new_target_position[1] = new_target_position[1] - move
+                if new_target_position[1] - shift >= self.start_position[1]:
+                    new_target_position[1] = new_target_position[1] - shift
                 elif self.prev_target_arm == "left":
                     new_target_position = self.left_reset_position.copy()
                 elif self.prev_target_arm == "right":
@@ -3026,14 +3015,14 @@ class TAgent(ResetableAgent):
                         wrong_arm = True
                     else:
                         new_target_position[0] = min(
-                            mid_pt_x, new_target_position[0] + move
+                            mid_pt_x, new_target_position[0] + shift
                         )
                 elif self.target_arm == "right" or self.prev_target_arm == "right":
                     if new_target_position[0] < mid_pt_x:
                         wrong_arm = True
                     else:
                         new_target_position[0] = max(
-                            mid_pt_x, new_target_position[0] - move
+                            mid_pt_x, new_target_position[0] - shift
                         )
                 else:
                     raise RuntimeError(
@@ -3042,7 +3031,7 @@ class TAgent(ResetableAgent):
                     )
                 if wrong_arm:
                     raise RuntimeError(
-                        "Cannot move target position backwards, as it is not on the "
+                        "Cannot shift target position backwards, as it is not on the "
                         "correct arm. Use set_target_position() first."
                     )
         else:
@@ -3051,7 +3040,7 @@ class TAgent(ResetableAgent):
                     left_edge = self.left_reset_position[0]
                     if new_target_position[0] > left_edge:
                         new_target_position[0] = max(
-                            left_edge, new_target_position[0] - move
+                            left_edge, new_target_position[0] - shift
                         )
                     else:
                         new_target_position = self.start_position.copy()
@@ -3059,7 +3048,7 @@ class TAgent(ResetableAgent):
                     right_edge = self.right_reset_position[0]
                     if new_target_position[0] < right_edge:
                         new_target_position[0] = min(
-                            right_edge, new_target_position[0] + move
+                            right_edge, new_target_position[0] + shift
                         )
                     else:
                         new_target_position = self.start_position.copy()
@@ -3071,7 +3060,7 @@ class TAgent(ResetableAgent):
                         "recognized."
                     )
             else:
-                new_target_position[1] = min(mid_pt_y, new_target_position[1] + move)
+                new_target_position[1] = min(mid_pt_y, new_target_position[1] + shift)
 
         self.set_target_position(new_target_position)
 
@@ -3258,7 +3247,7 @@ class OpenFieldAgent(ResetableAgent):
     Must be initialised with an environment. A parameters dictionary can also be passed.
 
     default_params = {
-        "reward_factor": 5,  # factor for setting a reward object as a target for a trajectory
+        "reward_factor": 3,  # factor for setting a reward object as a target for a trajectory
         "no_target_factor": 1,  # factor for not setting any target for a trajectory
         "trajectory_length": 2000,  # int or iterable of ints
         "num_trajectories": 10,  # number of trajectory lengths to sample
@@ -3298,11 +3287,12 @@ class OpenFieldAgent(ResetableAgent):
     """
 
     default_params = {
-        "reward_factor": 5,  # factor for setting a reward object as a target for a trajectory
+        "reward_factor": 3,  # factor for setting a reward object as a target for a trajectory
         "no_target_factor": 1,  # factor for not setting any target for a trajectory
         "trajectory_length": 2000,  # int or iterable of ints
         "num_trajectories": 10,  # number of trajectory lengths to sample
         "num_random_walk_steps": 100,  # number of steps to random walk, if target is not in sight
+        "wait_between_same_target": 300,  # number of steps to wait before a target can be reached a second time
         "always_log_teleportation": False,  # whether to log teleportation events when they occur
     }
 
@@ -3427,22 +3417,34 @@ class OpenFieldAgent(ResetableAgent):
         """
         self._set_random_walk()
 
-        If there is no current target or it is not in sight, starts a random walk and
-        adds it to the current line of the target dataframe. Otherwise, sets the number
-        of random walk steps to 0.
+        Sets a random walk in one of two cases:
+        1) There is no current target or the current target is not in sight.
+        2) There is current number of steps to wait before checking for the target.
+
+        If a random walk is on-going, an error is raised.
 
         Attributes:
         - current_num_of_random_walk_steps (int): Number of steps in the current
             random walk.
         """
 
+        if not hasattr(self, "current_num_of_random_walk_steps"):
+            self.current_num_of_random_walk_steps = 0
+
+        if self.current_num_of_random_walk_steps > 0:
+            raise RuntimeError("Random walk already in progress.")
+
         if self.target_position is None or not self.check_if_target_is_in_sight():
             self.current_num_of_random_walk_steps = int(self.num_random_walk_steps)  # type: ignore[attr-defined]
+        elif self.steps_before_checking_for_target > 0:
+            self.current_num_of_random_walk_steps = int(
+                self.steps_before_checking_for_target
+            )
+
+        if self.current_num_of_random_walk_steps:
             df_idx = len(self.target_df) - 1
             column = "random_walk_periods"
             self.target_df.loc[df_idx, column].append([self.num_steps_total])  # type: ignore[has-method]
-        else:
-            self.current_num_of_random_walk_steps = 0
 
     def _end_random_walk(self):
         """
@@ -3512,7 +3514,7 @@ class OpenFieldAgent(ResetableAgent):
 
         Attributes:
         - steps_before_checking_for_target (int): Number of steps to wait before
-            checking for target.
+            checking for the target.
         - target_position (1D np.ndarray): Current target position for the agent.
 
         Args:
@@ -3563,14 +3565,20 @@ class OpenFieldAgent(ResetableAgent):
         new_idx = len(self.target_df)
         self.target_df.loc[new_idx] = target_data  # type: ignore[assignment]
 
-        self.steps_before_checking_for_target = 0
-
+        # set target position
         if target_row["object_type_name"] == "no_target":
             self.target_position = None
         else:
             self.target_position = np.asarray(
                 [target_data["position_x"], target_data["position_y"]]
             )
+
+        # skip pause between targets if new target
+        if len(self.target_df) > 1 and (
+            self.target_df.loc[new_idx, "object_idx"]
+            != self.target_df.loc[new_idx - 1, "object_idx"]
+        ):
+            self.steps_before_checking_for_target = 0
 
     def _init_target_df(self):
         """
@@ -3639,6 +3647,7 @@ class OpenFieldAgent(ResetableAgent):
         - manual_pos (bool): Whether the agent's position was set manually.
         - start_position (1D np.ndarray): Start position of the agent.
         - steps_before_checking_for_target (int): Number of steps to wait before
+            checking for the target again.
         - target_position (1D np.ndarray): Target position of the agent.
 
         Args:

predhpc/neurons/riab_neurons.py
@@ -952,8 +952,8 @@ class PlaceCells(NeuronsMixin, riabPlaceCells):
         if self.description != "gaussian":
             raise ValueError("FWHM is only available for gaussian place fields.")
 
-        FWHM = self.place_cell_widths * np.sqrt(8 * np.log(2))
-
+        factor = 2 * np.sqrt(2 * np.log(2))  # approx 2.3548
+        FWHM = self.place_cell_widths * factor
         if average:
             FWHM = np.mean(FWHM)
 

predhpc/paper.py
@@ -119,7 +119,7 @@ def get_linear_Pyrs(
     scale=params_util.SCALE_LINEAR,
     speed_mean=params_util.SPEED_MEAN_LINEAR,
     speed_std=params_util.SPEED_STD,
-    wait_at_end=0,
+    wait_after_trajectory=0,
     log_BTSP=True,
     seed=True,
 ):
@@ -134,8 +134,8 @@ def get_linear_Pyrs(
         params_util.SPEED_MEAN_LINEAR.
     - speed_std (float): Standard deviation of the agent's speed. Default is
         params_util.SPEED_STD.
-    - wait_at_end (int): Number of steps to wait at the end of the environment.
-        Default is 0.
+    - wait_after_trajectory (int): Number of steps to wait after completing a
+        trajectory. Default is 0.
     - log_BTSP (bool): Whether to log BTSP events. Default is True.
     - seed (bool): Whether to seed the random number generator with the paper seed.
         Default is True.
@@ -160,7 +160,7 @@ def get_linear_Pyrs(
         target_position=target_position,
         speed_mean=speed_mean,
         speed_std=speed_std,
-        wait_at_end=wait_at_end,
+        wait_after_trajectory=wait_after_trajectory,
     )
 
     PC_params = params_util.get_PC_params(
@@ -252,11 +252,13 @@ def run_linear(
     Args:
     - Pyrs (Pyr, optional): Pyr object with initialized parameters. If None,
         a new Pyr object is created with default parameters.
-    - max_num_steps (int): Maximum number of steps to run the environment.
-        Default is 3800.
+    - max_num_steps (int): Maximum number of steps to run the environment. Note that
+        if learner is set to finish final trajectory, max_num_steps will be exceeded
+        to complete any incomplete trajectories. Default is 3800.
     - max_time_min (float, optional): Maximum time in minutes to run the environment.
-        If specified, it overrides max_num_steps based on the agent's time step.
-        Default is None.
+        If specified, it overrides max_num_steps based on the agent's time step. Note
+        that if learner is set to finish final trajectory, max_time_min will be
+        exceeded to complete any incomplete trajectories. Default is None.
     - BTSP_on (int): Trajectory on which to turn on BTSP. 1 for first trajectory.
         Default is None.
     - seed (bool): Whether to seed the random number generator with the paper seed.
@@ -271,7 +273,9 @@ def run_linear(
         gen_util.seed_all(PAPER_SEED)
 
     if Pyrs is None:
-        Pyrs = get_linear_Pyrs(seed=False, wait_at_end=int(15 / params_util.DT))
+        Pyrs = get_linear_Pyrs(
+            seed=False, wait_after_trajectory=params_util.WAIT_LINEAR
+        )
 
     if max_time_min is not None:
         max_num_steps = int(max_time_min * 60 / Pyrs.Agent.dt)
@@ -283,7 +287,7 @@ def run_linear(
     return learner
 
 
-def plot_linear_summary(learner=None, max_time_min=1.8, **kwargs):
+def plot_linear_summary(learner=None, max_time_min=2.0, **kwargs):
     """
     plot_linear_summary()
 
@@ -292,8 +296,9 @@ def plot_linear_summary(learner=None, max_time_min=1.8, **kwargs):
     Args:
     - learner (Learner): Learner object.
     - max_time_min (float, optional): Maximum time in minutes to run the environment.
-        If specified, it overrides max_num_steps based on the agent's time step.
-        Default is 1.8.
+        If specified, it overrides max_num_steps based on the agent's time step. Note
+        that if learner is set to finish final trajectory, max_time_min will be
+        exceeded to complete any incomplete trajectories. Default is 2.0.
 
     Keywords args:
     - **kwargs: Additional keyword arguments passed to
@@ -311,7 +316,7 @@ def plot_linear_summary(learner=None, max_time_min=1.8, **kwargs):
     return ax1D
 
 
-def plot_linear_place_fields(learner, max_time_min=1.8, **kwargs):
+def plot_linear_place_fields(learner, max_time_min=2.0, **kwargs):
     """
     plot_linear_place_fields(learner)
 
@@ -320,8 +325,9 @@ def plot_linear_place_fields(learner, max_time_min=1.8, **kwargs):
     Args:
     - learner (Learner): Learner object.
     - max_time_min (float, optional): Maximum time in minutes to run the environment.
-        If specified, it overrides max_num_steps based on the agent's time step.
-        Default is 1.8.
+        If specified, it overrides max_num_steps based on the agent's time step. Note
+        that if learner is set to finish final trajectory, max_time_min will be
+        exceeded to complete any incomplete trajectories. Default is 2.0.
 
     Keywords args:
     - **kwargs: Additional keyword arguments passed to
@@ -339,7 +345,7 @@ def plot_linear_place_fields(learner, max_time_min=1.8, **kwargs):
     return ax1D
 
 
-def plot_linear_binned_rates(learner, max_time_min=1.8, **kwargs):
+def plot_linear_binned_rates(learner, max_time_min=2.0, **kwargs):
     """
     plot_linear_binned_rates(learner)
 
@@ -348,8 +354,9 @@ def plot_linear_binned_rates(learner, max_time_min=1.8, **kwargs):
     Args:
     - learner (Learner): Learner object.
     - max_time_min (float, optional): Maximum time in minutes to run the environment.
-        If specified, it overrides max_num_steps based on the agent's time step.
-        Default is 1.8.
+        If specified, it overrides max_num_steps based on the agent's time step. Note
+        that if learner is set to finish final trajectory, max_time_min will be
+        exceeded to complete any incomplete trajectories. Default is 2.0.
 
     Keywords args:
     - **kwargs: Additional keyword arguments passed to
@@ -388,8 +395,10 @@ def run_linear_speed(
     - speed_mean (float): Mean speed for the experiment.
         Default is params_util.SPEED_MEAN_LINEAR.
     - i (int): Index for the experiment run. Default is 0.
-    - max_time_min (float): Maximum time in minutes to run the environment for
-        assessing place field. Default is NUM_TRAJ_SPEED.
+    - max_time_min (float, optional): Maximum time in minutes to run the environment
+        for assessing place field. Note that if learner is set to complete all
+        trajectories, max_time_min will be exceeded to complete any incomplete
+        trajectories. Default is NUM_TRAJ_SPEED.
     - max_num_traj (int): Maximum number of trajectories to run for assessing place
         field. Default is NUM_TRAJ_SPEED.
     - BTSP_on (int): Trajectory on which to enable. Later trajectories allow more
@@ -417,6 +426,7 @@ def run_linear_speed(
         - "PF_widths": Last place field widths.
         - "num_BTSP_applied": Number of BTSP events that were applied in total for
             each target shift.
+        - "max_norm_value": Maximum weight normalization value used.
         if seed:
         - "seed": Seed for the experiment.
     """
@@ -429,7 +439,7 @@ def run_linear_speed(
         speed_mean=speed_mean,
         speed_std=speed_std,
         log_BTSP=False,
-        wait_at_end=0,
+        wait_after_trajectory=0,
         seed=False,
     )
 
@@ -465,10 +475,15 @@ def run_linear_speed(
 
     data_dict = gather_PF_info(learner, k=k, t_start=t_start)
 
-    data_dict["speed_mean"] = speed_mean
-    data_dict["num_BTSP_applied"] = len(
+    num_BTSP_applied = len(
         Pyrs.SomaticCompartment.get_BTSP_steps(applied_only=True, apply_step=True)
     )
+    norm_values = learner.Pyrs.SomaticCompartment.get_normalization_values("PCs")[1]
+    max_norm_value = norm_values.max() if len(norm_values) > 0 else np.nan
+
+    data_dict["speed_mean"] = speed_mean
+    data_dict["num_BTSP_applied"] = num_BTSP_applied
+    data_dict["max_norm_value"] = max_norm_value
 
     if seed:
         data_dict["seed"] = seed_value
@@ -488,8 +503,9 @@ def run_linear_speeds(
     Args:
     - seed (bool): Whether to seed the random number generator with the paper seed.
         Default is True.
-    - max_time_min (float): Maximum time in minutes to run the environment.
-        Default is NUM_TRAJ_SPEED.
+    - max_time_min (float): Maximum time in minutes to run the environment for. Note
+        that if learner is set to complete all trajectories, max_time_min will be
+        exceeded to complete any incomplete trajectories. Default is NUM_TRAJ_SPEED.
     - num_repeats (int): Number of repeats for the experiment. Default is 1.
     - k (int): Smoothing factor for measuring place field width from firingrate history.
         Default is SMOOTH_K.
@@ -507,6 +523,7 @@ def run_linear_speeds(
         - "PF_centers": Array of place field centers.
         - "PF_widths": Array of place field widths.
         - "num_BTSP_applied": Number of BTSP events applied for each speed.
+        - "max_norm_values": Maximum weight normalization value used for each speed.
         if seed:
         - "seeds": Array of seeds for each run.
     """
@@ -549,6 +566,7 @@ def run_linear_speeds(
                 [speed_dict[key] for speed_dict in speed_dicts]
             )
     speed_data["speed_means"] = speed_data.pop("speed_mean")
+    speed_data["max_norm_values"] = speed_data.pop("max_norm_value")
 
     if "seed" in speed_data.keys():
         speed_data["seeds"] = speed_data.pop("seed")
@@ -661,8 +679,9 @@ def run_linear_shift(
     - i (int): Index for the experiment run. Default is 0.
     - speed_std (float): Standard deviation of speed for the experiment.
         Default is 0.
-    - max_time_min (float): Maximum time in minutes to run the environment for
-        assessing place field. Default is 5.
+    - max_time_min (float): Maximum time in minutes to run the environment for. Note
+        that if learner is set to complete all trajectories, max_time_min will be
+        exceeded to complete any incomplete trajectories. Default is 5.
     - max_num_traj (int): Maximum number of trajectories to run for assessing place
         field. Default is 5.
     - BTSP_on (int): Trajectory on which to enable. Later trajectories allow more
@@ -678,7 +697,7 @@ def run_linear_shift(
 
     Returns:
     - data_dict (dict): Dictionary containing the results of the experiment under keys:
-        - "speed_mean": Mean speed for the experiment.
+        - "target_shift": Target shift for the experiment.
         - "PC_place_centers": Place cell centers.
         - "PC_weights": Place cell input weights.
         - "PC_weight_widths": Last place cell input weight widths.
@@ -689,6 +708,7 @@ def run_linear_shift(
         - "PF_widths": Last place field widths.
         - "num_BTSP_applied": Number of BTSP events that were applied in total for the
             neuron layer.
+        - "max_norm_value": Maximum weight normalization value used.
         if seed:
         - "seed": Seed for the experiment.
     """
@@ -721,7 +741,7 @@ def run_linear_shift(
         warnings.filterwarnings(
             "ignore", category=UserWarning, message="Target position"
         )
-        learner.Pyrs.Agent.move_target_position(target_shift)
+        learner.Pyrs.Agent.shift_target_position(target_shift)
 
     for i in range(5):
         learner = run_linear(
@@ -788,8 +808,12 @@ def run_linear_shift(
                 [initial_shift_dict[PF_key][:1], data_dict[PF_key]], axis=0
             )
 
+    norm_values = learner.Pyrs.SomaticCompartment.get_normalization_values("PCs")[1]
+    max_norm_value = norm_values.max() if len(norm_values) > 0 else np.nan
+
     data_dict["target_shift"] = target_shift
     data_dict["num_BTSP_applied"] = num_BTSP_applied
+    data_dict["max_norm_value"] = max_norm_value
 
     if seed:
         data_dict["seed"] = seed_value
@@ -807,8 +831,9 @@ def run_linear_shifts(seed=True, max_time_min=5, num_repeats=1, k=SMOOTH_K, num_
     Args:
     - seed (bool): Whether to seed the random number generator with the paper seed.
         Default is True.
-    - max_time_min (float): Maximum time in minutes to run the environment.
-        Default is 5.
+    - max_time_min (float): Maximum time in minutes to run the environment for. Note
+        that if learner is set to complete all trajectories, max_time_min will be
+        exceeded to complete any incomplete trajectories. Default is 5.
     - num_repeats (int): Number of repeats for the experiment. Default is 1.
     - k (int): Smoothing factor for measuring place field width from firingrate history.
         Default is SMOOTH_K.
@@ -827,6 +852,7 @@ def run_linear_shifts(seed=True, max_time_min=5, num_repeats=1, k=SMOOTH_K, num_
         - "PF_widths": Array of place field widths.
         - "num_BTSP_applied": Number of BTSP events that were applied in total for
             each target shift.
+        - "max_norm_value": Maximum weight normalization value used for each target shift.
         if seed:
         - "seeds": Array of seeds for each run.
     """
@@ -883,6 +909,8 @@ def run_linear_shifts(seed=True, max_time_min=5, num_repeats=1, k=SMOOTH_K, num_
         shift_dict[key] = np.concatenate([initial_data, shift_dict[key]], axis=1)
 
     shift_dict["target_shifts"] = shift_dict.pop("target_shift")
+    shift_dict["max_norm_values"] = shift_dict.pop("max_norm_value")
+
     if seed:
         shift_dict["seeds"] = np.full(len(target_shifts), PAPER_SEED)
 
@@ -972,6 +1000,36 @@ def plot_target_shift_PFs(
     return ax1D
 
 
+def log_max_normalization_value(norm_values):
+    """
+    log_max_normalization_value(norm_values)
+
+    Logs the maximum weight normalization value recorded.
+
+    Args:
+    - norm_values (np.ndarray): Array of normalization values to log.
+    """
+
+    finite = np.isfinite(norm_values)
+    if finite.any():
+        max_norm = np.nanmax(norm_values)
+        if max_norm > 1:
+            log_str = f"Max. weight normalization value applied: {max_norm:.4f}"
+            n = (norm_values[finite] > 1).sum()
+            if finite.sum() > 1:
+                log_str = f"{log_str} ({n}/{finite.sum()} values > 1)."
+            else:
+                log_str = f"{log_str}."
+        else:
+            log_str = (
+                "No weight normalization applied "
+                f"(max value of {max_norm:.4f} <= 1)."
+            )
+    else:
+        log_str = "No weight normalization values found."
+    print(log_str)
+
+
 def run_linear_fct(fct_name="linear_speeds", overwrite=False, seed=True, num_jobs=1):
     """
     run_linear_fct()
@@ -1010,6 +1068,9 @@ def run_linear_fct(fct_name="linear_speeds", overwrite=False, seed=True, num_job
         data_dict = fct(seed=seed, num_jobs=num_jobs)
         gen_util.save_np_dict(save_path, data_dict)
         gen_util.get_duration_str(start_time, log=True)
+
+    if "max_norm_values" in data_dict.keys():
+        log_max_normalization_value(data_dict["max_norm_values"])
 
     return data_dict
 
@@ -1070,11 +1131,13 @@ def run_openfield_corridor(
     Args:
     - Pyrs (Pyr, optional): Pyr object with initialized parameters. If None,
         a new Pyr object is created with default parameters.
-    - max_num_steps (int): Maximum number of steps to run the environment.
-        Default is 3800.
+    - max_num_steps (int): Maximum number of steps to run the environment. Note
+        that if learner is set to finish final trajectory, max_num_steps will be
+        exceeded to complete any incomplete trajectories. Default is 3800.
     - max_time_min (float, optional): Maximum time in minutes to run the environment.
-        If specified, it overrides max_num_steps based on the agent's time step.
-        Default is None.
+        If specified, it overrides max_num_steps based on the agent's time step. Note
+        that if learner is set to finish final trajectory, max_time_min will be
+        exceeded to complete any incomplete trajectories. Default is None.
     - seed (bool): Whether to seed the random number generator with the paper seed.
         Default is True.
 

predhpc/paper_plot_fcts.py
@@ -14,6 +14,9 @@ from predhpc.util import gen_util, params_util, plot_util, ext_util
 
 LW = 1.6
 
+BTSP_ASTERISK = (5, 2, 0)  # asterisk
+BTSP_S = 35  # size of BTSP markers
+
 
 def stylize_plots_for_paper(fs=12, lw=1.7, tick_length=6, notebook=False):
     """
@@ -352,7 +355,7 @@ def plot_single_neuron_rate_timeseries(
     sub_ax.set_ylim(ymin, ymax)
 
     NeuronLayer.add_BTSP_markers_to_plots(
-        ax=sub_ax, s=35, prop_y=0.8, lw=lw, marker=(5, 2, 0), timeseries=True
+        ax=sub_ax, s=BTSP_S, prop_y=0.8, lw=lw, marker=BTSP_ASTERISK, timeseries=True
     )
     sub_ax.set_ylabel("")
     plot_util.expand_ticks(sub_ax, axis="x", num_ticks=7, alternating=True, round_dec=1)
@@ -452,8 +455,8 @@ def plot_linear_summary(learner):
     _, _, PCs, _ = ext_util.extract_objects_from_Pyrs(learner.Pyrs)
 
     plot_kwargs = {
-        "hspace": 0.35,
-        "height_ratios": [0.1, 0.1, 0.4, 0.14, 0.14, 0.14],
+        "hspace": 0.37,
+        "height_ratios": [0.1, 0.1, 0.42, 0.14, 0.14, 0.14],
         "figsize": (5.8, 8.3),
         "lw": LW,
         "s": 1.2,
@@ -461,9 +464,9 @@ def plot_linear_summary(learner):
     }
 
     Pyr_kwargs = {
-        "BTSP_s": 35,
+        "BTSP_s": BTSP_S,
         "BTSP_lw": LW,
-        "BTSP_marker": (5, 2, 0),  # asterisk
+        "BTSP_marker": BTSP_ASTERISK,
         "separate_axes": True,
         "no_legend": True,
     }
@@ -502,6 +505,7 @@ def plot_linear_summary(learner):
             lw=plot_kwargs["lw"],
             loc=(0.725, 0.65),
             handlelength=0.8,
+            handletextpad=0.5,
             frameon=False,
             fontsize=11,
         )
@@ -1566,6 +1570,71 @@ def plot_openfield_components(Pyrs, titles=False):
     return axes
 
 
+def get_s_openfield_PFs(fig_side=4.0, PF_type="weights"):
+    """
+    get_s_openfield_PFs(fig_side=4.0, num_x=60)
+
+    Returns the place field square size (s) needed to contiguously plot openfield place
+    fields without overlap, based on the figure size and type of place field to plot.
+
+    For weights, expects the place field plot to consist of a square grid of 60x60
+    input place fields. For history, expects a square grid of 120x120 positions.
+
+    Sizes were identified manually.
+
+    Args:
+    - fig_side (float, optional): Size of the figure. Default is 4.
+    - PF_type (str, optional): PF evaluation method to plot. Default is "weights".
+
+    Returns:
+    - s (float): Suggested starting estimate for the place field square size.
+    """
+
+    approx = False
+    if "weights" in PF_type:
+        if fig_side == 1:
+            s = 10.2
+        elif fig_side == 2:
+            s = 42.8
+        elif fig_side == 3:
+            s = 99
+        elif fig_side == 4:
+            s = 176.3
+        elif fig_side == 5:
+            s = 278
+        else:
+            s = max(1, 66.8 * np.exp(fig_side * 0.34) - 85.6)
+            approx = True
+
+    elif PF_type == "history":
+        if fig_side == 1:
+            s = 2.55
+        elif fig_side == 2:
+            s = 10.7
+        elif fig_side == 3:
+            s = 24.5
+        elif fig_side == 4:
+            s = 44.08
+        elif fig_side == 5:
+            s = 69
+        else:
+            s = max(1, 17 * np.exp(fig_side * 0.34) - 21.8)
+            approx = True
+
+    else:
+        raise NotImplementedError(
+            f"No proposed place field square size available for {PF_type} PF_type."
+        )
+
+    if approx:
+        print(
+            f"No place field square size recorded for fig_side of {fig_side}. "
+            f"Starting estimate of {s} suggested."
+        )
+
+    return s
+
+
 def plot_openfield_PFs(Pyrs, fig_side=4.0, lw=LW, alpha=0.8, PF_type="weights"):
     """
     plot_openfield_PFs(Pyrs)
@@ -1584,21 +1653,23 @@ def plot_openfield_PFs(Pyrs, fig_side=4.0, lw=LW, alpha=0.8, PF_type="weights"):
 
     fig, sub_ax = plt.subplots(figsize=(fig_side, fig_side))
 
+    if Pyrs.SomaticCompartment.n != 1:
+        raise ValueError("Only single neuron plotting is supported.")
+
     PF_t_start = None
     if PF_type == "history":
         round_dec = 0
-        s = 44.08  # matched to default fig side
         BTSP_applied = Pyrs.SomaticCompartment.get_BTSP_steps(
             applied_only=True, apply_step=True
         )
         vmax = 10
         if len(BTSP_applied):
             PF_t_start = BTSP_applied[-1] * Pyrs.SomaticCompartment.Agent.dt
-
     else:
         round_dec = 2
-        s = 176.3  # matched to default fig side
         vmax = None
+
+    s = get_s_openfield_PFs(fig_side=fig_side, PF_type=PF_type)
 
     plot_fcts.plot_2D_PFs(
         Pyrs.SomaticCompartment,

predhpc/util/signal_util.py
@@ -133,7 +133,7 @@ def get_interp_x(x, partway_idx, max_x=None):
         if max_x is None:
             right_pos = left_pos + x[0]
         else:
-            right_pos = max_x + x[0]
+            right_pos = max_x
 
     else:
         right_pos = x[idx + 1]
@@ -230,8 +230,17 @@ def compute_signal_width(
 
     if x is None:
         x = np.arange(len(signal))
+        if max_x is None:
+            max_x = len(signal)
+    elif max_x is None:
+        max_x = x.max()
 
-    elif len(signal) != len(x):
+    if x.max() > max_x:
+        raise RuntimeError(f"max_x ({max_x}) is smaller than x max ({x.max()}).")
+    elif x.min() < 0:
+        raise RuntimeError(f"min x ({x.min()}) cannot be negative.")
+
+    if len(signal) != len(x):
         raise ValueError("Signal and x must have the same length.")
 
     sorter = np.argsort(x)
@@ -247,20 +256,18 @@ def compute_signal_width(
     left_idx = get_partway_idx_of_prop_max_crossing_on_right(
         smoothed_signal[::-1], prop_peak=prop_peak
     )
-
-    if max_x is None:
-        max_x = x.max()
+    left_idx = len(x) - 1 - left_idx
 
     width_edges = np.full(2, np.nan)
     if np.isnan(right_idx) or np.isnan(left_idx):
         width = max_x
 
-    elif np.isclose(right_idx, len(x) - 1 - left_idx):
+    elif np.isclose(right_idx, left_idx):
         width = max_x
 
     else:
         right_pos = get_interp_x(x, right_idx, max_x=max_x)
-        left_pos = get_interp_x(x[::-1], left_idx, max_x=max_x)
+        left_pos = get_interp_x(x, left_idx, max_x=max_x)
 
         if right_pos == left_pos:
             width = max_x
@@ -1150,7 +1157,6 @@ def get_2D_Gaussian_kernel(sigma, aperture_prop=8, max_aperture=np.inf):
     """
 
     aperture = int(np.ceil(min(sigma * aperture_prop, max_aperture)) // 2 * 2 + 1)
-    print(aperture)
 
     x = np.linspace(-(aperture // 2), aperture // 2, aperture)
     xx, yy = np.meshgrid(x, x)